532dfaf5c04a698c165042ccfca1eace76e0fde4
Skyler Sidner <malevolentdragon@gmail.com>
2015-01-27 09:16:09 -0600

    Added comments

--- split.rb
+++ split.rb
@@ -1,8 +1,11 @@
+# Used so we can halt and interact with the program
 require 'pry'
 
 class Split
+  # Enables get/set for each of these method calls
   attr_accessor :cost, :percent, :guests
   
+  # Deamnds parameters, instance variable declaration and initial operations
   def initialize(cost, percent, guests)
     @cost = cost
     @percent = (percent.to_f / 100)
@@ -11,6 +14,7 @@ class Split
     @total_cost = (@cost + (@tip))
   end
 
+  # Methods to call specifics of operations
   def tip
     perc = (@percent * 100)
     puts "The tip at #{perc} percent would be $%.2f." % @tip
@@ -20,9 +24,11 @@ class Split
     puts "The total cost, with tip, would be $%.2f." % @total_cost
   end
   
+  # Solution
   def per_person
     puts "Split evenly, the cost per person would be $%.2f." % (@total_cost / @guests)
   end
 end
 
+# Halting command
 binding.pry
