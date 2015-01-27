0a3d1d13d9a977954bef57d9cd9ada42c0d9a460
Skyler Sidner <malevolentdragon@gmail.com>
2015-01-26 18:10:03 -0600

    First commit; foundation of methods

---
+++ 2015-01-26-checksplitter.rb
@@ -0,0 +1,42 @@
+Class Checksplitter
+  def initialize(par1)
+    @check = par1
+    @percent = 15
+  end
+
+  def set_group_num(x)
+    @group_num = x
+  end
+  
+  def get_group_num
+    @group_num
+  end
+  
+  def set_percent(x)
+    @percent = x
+  end
+  
+  def get_percent
+    @percent
+  end
+  
+  def set_check(x)
+    @check = x
+  end
+  
+  def get_check
+    @check
+  end
+  
+  def get_tip
+    @tip = (@check * (@percent / 100))
+  end
+  
+  def get_total
+    @total = (@check + @tip)
+  end
+  
+  def get_per_person
+    @per_person = (@total / @groupnum)
+    puts "Split evenly, each person owes $#{@per_person}."
+  end
