1b135cc381dbedc90d284d47ea34b3b4a606dfff
Skyler Sidner <malevolentdragon@gmail.com>
2015-01-27 22:57:12 -0600

    Almost finished DinnerEvent Class

--- split.rb
+++ split.rb
@@ -20,7 +20,7 @@ require 'pry'
 # #per_person
 
 
-class Split
+class SplitCheck
   #EXAMPLE!!!!!!!!
   # Public: #play
   # Starts and then plays the game with the provided players.
@@ -39,15 +39,6 @@ class Split
 
 
 
-  # Public: @attr_accessor
-  # Creates get/set methods for #cost, #percent, #guests
-  # 
-  # Parameters:
-  # 
-  # Returns:
-  #
-  # State Changes:
-  #
   attr_accessor :cost, :percent, :guests
   
   # Public: #initialize
@@ -109,48 +100,97 @@ class Split
   #
 
   def per_person
-    total_cost / @guests
+    (total_cost / @guests).round(2)
   end
 end
 
 # New class to interact with Split
 class DinnerClub
-  def initialize(club_name)
-    @club_name = club_name
-    @members = {}
+  def initialize(name)
+    @name = name
+    @members = []
+    @member_totals = {}
   end
   
   def new_member(name)
-    @members[name] = 0.0
+    @members << name
+    @member_totals[name] = 0.0
   end
   
+  # @members.each { |key|
+  #   @member_totals[key] = 0.0
+  # }
+    
+
+  # Mostly for testing
   def list_members
     @members
+  end
+  
+  # Mostly for testing
+  def list_totals
+    @member_totals
   end
   
   def members_present
     @members.count
   end
-  
-  # Method to create a new split check
-  def new_dinner(cost, percent)
-    guests = members_present
-    @check = Split.new(cost, percent, guests)
+end
+
+
+class DinnerEvent
+  def initialize(name, cost, percent, guests)
+    @event = name
+    @check = SplitCheck.new(cost, percent, guests)
+    @event_log = {}
+    @event_log[name] = ""
+    @who_attended = []
   end
   
-  def cost_to_members
-    @check.per_person.round(2)
+  def attended(x)
+      @who_attended << x
   end
+  
+  def cost_to_attending
+    @check.per_person
+  end
+  
   
   # Adds to each member's total
-  def charge_em
-    @members.each { |key, value|
-      @members[key] = value += cost_to_members
+  # WARNING - THIS CURRENTLY IS NOT WORKING - 1-27-2015
+  def log_event
+    @who_attended.each { |name|
+      x = @member_totals[name]
+      @member_totals[name] = (x += cost_to_attending)
     }
+    @event_log[@event] = @who_attended
   end
 end
 
 
-    
+
+
+# Test statement
+check = SplitCheck.new(42.42, 17, 3)
+puts check.tip
+puts check.total_cost
+puts check.per_person
+
+club = DinnerClub.new("Club Dragon")
+club.new_member("Bob")
+club.new_member("Sue")
+club.new_member("Joe")
+club.new_member("Ian")
+club.new_member("Sam")
+club.list_members
+club.list_totals
+
+event = DinnerEvent.new("Blue Sushi", 83.71, 18, 4)
+event.attended("Bob")
+event.attended("Sue")
+event.attended("Ian")
+event.attended("Sam")
+
+
 # Halting command
 binding.pry
